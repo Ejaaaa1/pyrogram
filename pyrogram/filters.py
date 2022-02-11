aa93d16987ffaaf9d6bb61de9df9eb823ea6a668
Andrea Princic <48788808+Princic-1837592@users.noreply.github.com>
2022-02-11 13:30:34 -0300

    Added CallbackQuery support to other filters
    Squashed commit of the following:
    
    commit ae780457a874dbc4fb8b5946611fd1b34e1bdbdd
    Author: Andrea Princic <48788808+Princic-1837592@users.noreply.github.com>
    Date:   Thu Feb 3 15:03:23 2022 +0100
    
        Added CallbackQuery support to other filters
    
    commit 7ff04657c88c48b62705ba38a639e2d38ac076ce
    Author: Andrea Princic <48788808+Princic-1837592@users.noreply.github.com>
    Date:   Sat Jan 29 19:08:14 2022 +0100
    
        Now filters.group supports callback queries

--- pyrogram/filters.py
+++ pyrogram/filters.py
@@ -428,8 +428,9 @@ dice = create(dice_filter)
 # endregion
 
 # region private_filter
-async def private_filter(_, __, m: Message):
-    return bool(m.chat and m.chat.type in {enums.ChatType.PRIVATE, enums.ChatType.BOT})
+async def private_filter(_, __, u: Update):
+    m = u.message if isinstance(u, CallbackQuery) else u
+    return bool(m and m.chat and m.chat.type in {enums.ChatType.PRIVATE, enums.ChatType.BOT})
 
 
 private = create(private_filter)
@@ -439,8 +440,9 @@ private = create(private_filter)
 # endregion
 
 # region group_filter
-async def group_filter(_, __, m: Message):
-    return bool(m.chat and m.chat.type in {enums.ChatType.GROUP, enums.ChatType.SUPERGROUP})
+async def group_filter(_, __, u: Update):
+    m = u.message if isinstance(u, CallbackQuery) else u
+    return bool(m and m.chat and m.chat.type in {enums.ChatType.GROUP, enums.ChatType.SUPERGROUP})
 
 
 group = create(group_filter)
@@ -450,8 +452,9 @@ group = create(group_filter)
 # endregion
 
 # region channel_filter
-async def channel_filter(_, __, m: Message):
-    return bool(m.chat and m.chat.type == enums.ChatType.CHANNEL)
+async def channel_filter(_, __, u: Update):
+    m = u.message if isinstance(u, CallbackQuery) else u
+    return bool(m and m.chat and m.chat.type == enums.ChatType.CHANNEL)
 
 
 channel = create(channel_filter)
