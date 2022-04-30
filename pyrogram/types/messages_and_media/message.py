itzharshit/1.4.12-amn

Bound method to stop_poll

pyrogram/types/messages_and_media/message.py
@@ -3225,6 +3225,43 @@ class Message(Object, Update):
         else:
             await self.reply(button, quote=quote)
 
+    async def stop_poll(
+        self,
+        reply_markup: "types.InlineKeyboardMarkup" = None
+    ) -> "types.Poll":
+        """Bound method *stop_poll* of :obj:`~pyrogram.types.Message`.
+
+        Use as a shortcut for:
+
+        .. code-block:: python
+
+            client.stop_poll(
+                chat_id=message.chat.id,
+                message_id=message_id,
+            )
+
+        Parameters:
+            reply_markup (:obj:`~pyrogram.types.InlineKeyboardMarkup`, *optional*):
+                An InlineKeyboardMarkup object.
+
+        Example:
+            .. code-block:: python
+
+                message.stop_poll()
+
+        Returns:
+            :obj:`~pyrogram.types.Poll`: On success, the stopped poll with the final results is returned.
+
+        Raises:
+            RPCError: In case of a Telegram RPC error.
+        """
+
+        return await self._client.stop_poll(
+            chat_id=self.chat.id,
+            message_id=self.message_id,
+            reply_markup=reply_markup
+        )
+
     async def retract_vote(
         self,
     ) -> "types.Poll":

Ejaaaa1/pyrogram

pyrogram/types/messages_and_media/message.py
@@ -3346,6 +3346,35 @@ class Message(Object, Update):
             emoji=emoji
         )
 
+    async def stop_poll(
+        self,
+        reply_markup: "types.InlineKeyboardMarkup" = None
+    ) -> "types.Poll":
+        """Bound method *stop_poll* of :obj:`~pyrogram.types.Message`.
+        Use as a shortcut for:
+        .. code-block:: python
+            client.stop_poll(
+                chat_id=message.chat.id,
+                message_id=message_id,
+            )
+        Parameters:
+            reply_markup (:obj:`~pyrogram.types.InlineKeyboardMarkup`, *optional*):
+                An InlineKeyboardMarkup object.
+        Example:
+            .. code-block:: python
+                message.stop_poll()
+        Returns:
+            :obj:`~pyrogram.types.Poll`: On success, the stopped poll with the final results is returned.
+        Raises:
+            RPCError: In case of a Telegram RPC error.
+        """
+
+        return await self._client.stop_poll(
+            chat_id=self.chat.id,
+            message_id=self.id,
+            reply_markup=reply_markup
+        )
+
     async def retract_vote(
         self,
     ) -> "types.Poll":